357aab52b02b726a3c3bbe95f5f0ea27c885413f
Maximiliano <maxigomez.udaondo@gmail.com>
2024-10-03 13:51:14 -0300

    sss

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -88,11 +88,12 @@ function Content(props){
     return (
         <div className="Content">
             <section class="banner" id="home">
-            <h1 className='animate__animated animate__fadeIn animate__delay-1s'>Maximiliano Gomez</h1>
-                <div className='typewriter'>
-                <p>Desarrollador web.<span>&#160;</span></p>
-                </div>
-                <div class="lds-ellipsis animate__animated animate__fadeOut animate__delay-2s"><div></div><div></div><div></div><div></div></div>
+            <div className='texto-banner'>
+            <span className='hola'>HOLA SOY</span>
+            <h1 className='animate__animated animate__fadeIn animate__delay-1s'>GOMEZ <br></br>MAXIMILIANO</h1>
+                <p>&lt;Desarrollador web/&gt;<span>&#160;</span></p>
+                {/* <div class="lds-ellipsis animate__animated animate__fadeOut animate__delay-2s"><div></div><div></div><div></div><div></div></div> */}
+            </div>
             </section>
 
             <div class="nosotros-bk">
