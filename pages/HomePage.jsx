0568188f9eb6b85f33eb479a48f18902c0f54771
Maximiliano <maxigomez.udaondo@gmail.com>
2026-08-01 19:03:36 -0300

    videos proyectos

--- pages/HomePage.jsx
+++ pages/HomePage.jsx
@@ -14,24 +14,48 @@ function HomePage(){
         })
     }, [])
 
-   return ( 
-            <div className="container">
-            <div className="row justify-content-center">
-                {proyectos.map(({_id, name, img,link, descripcion}) =>
-                <div key={_id} className="col-md-6 contenedor-proyecto">
-                <a target={"_blank"} href={`${link}`}>
-                    <div className="contenedor-img"> 
-                    <img src={`${img}`}></img>
-                    </div>
-                    <h3>{name}</h3>
-                    <p>{descripcion}</p> 
-                    <a class="btn btn-primary" target={"_blank"} href={`${link}`}>Visitar sitio</a>
-                </a>                  
-                </div>
-                )}
-            </div>
-            </div> 
-          
-          )}
+   return (
+  <div className="container">
+    {proyectos.map(({ _id, name, video, link, descripcion }) => (
+      <div key={_id} className="row align-items-center contenedor-proyecto mb-5">
+
+        {/* Video */}
+        <div className="col-12 col-lg-6">
+          <div className="contenedor-video">
+            <video
+              autoPlay
+              muted
+              loop
+              playsInline
+              preload="metadata"
+              className="video-proyecto"
+            >
+              <source src={video} type="video/mp4" />
+            </video>
+          </div>
+        </div>
+
+        {/* Información */}
+        <div className="col-12 col-lg-6">
+          <div className="info-proyecto">
+            <h3>{name}</h3>
+            <p>{descripcion}</p>
+
+            <a
+              className="btn btn-primary"
+              href={link}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Visitar sitio
+            </a>
+          </div>
+        </div>
+
+      </div>
+    ))}
+  </div>
+);
+}
 
 export default HomePage
